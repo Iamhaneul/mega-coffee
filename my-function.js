67d67b9916ea6fe2404edb5b90fcea28b1f4d42c
Iamhaneul <143384467+Iamhaneul@users.noreply.github.com>
2023-12-08 16:26:35 +0900

    Add files via upload

---
+++ my-function.js
@@ -0,0 +1,12 @@
+function setActive(el){
+  el.addClass('active');
+}
+function removeActive(el){
+  el.removeClass('active');
+}
+
+function setHeight(el,tHeight){
+  el.css({
+    height:tHeight,
+  })
+}
